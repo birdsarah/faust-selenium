1184190227f5ccddd23e353e9594300e15a4715d
Sarah Bird <birdsarah@mozilla.com>
2019-08-08 00:03:50 -0500

    Crawl with logger

--- cat-crawler/crawler.py
+++ cat-crawler/crawler.py
@@ -1,5 +1,8 @@
-import asyncio
+import time
 import uuid
+
+from selenium.common.exceptions import WebDriverException
+from selenium import webdriver
 
 from app import (
     app,
@@ -14,12 +17,21 @@ from app import (
 async def crawl(crawl_requests):
     async for crawl_request in crawl_requests:
         print(f'Receiving Request: {crawl_request.url}')
+
+        # Do a crawl
+        driver = webdriver.Firefox()
+        try:
+            driver.get(crawl_request.url)
+            time.sleep(2)
+        except WebDriverException as e:
+            logger.exception(e)
+        finally:
+            driver.close()
+
         result = CrawlResult(
             id=str(uuid.uuid4()),
             request_id=crawl_request.id,
             url=crawl_request.url,
             success=True
         )
-        await asyncio.sleep(0.2)
-        logger.info(f'THIS IS A LOG: Sending Result: {result.id}')
         await crawl_result_topic.send(value=result)
