195f8e466aa9a6526c9206f452d53fca829f6ec8
Sarah Bird <birdsarah@mozilla.com>
2019-10-02 01:45:25 -0500

    Base crawler

--- crawler/producer.py
+++ crawler/producer.py
@@ -21,7 +21,6 @@ async def producer():
         reader = csv.reader(f)
         sites = [item for sublist in list(reader) for item in sublist]
 
-    sites = sites
     for u in sites:
         url = quote(f'http://{u}', safe=":/?=")
         visit_id = (uuid.uuid4().int & (1 << 53) - 1) - 2**52
