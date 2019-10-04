c297551c7a86b6d1a5c91dfe115b8555142ec6a0
Sarah Bird <birdsarah@mozilla.com>
2019-10-04 14:09:58 -0500

    Offload slow-running crawl task

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -4,6 +4,8 @@ import pytz
 import time
 import uuid
 
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 from selenium.common.exceptions import WebDriverException, TimeoutException
 from urllib import parse
 
@@ -25,6 +27,43 @@ from browser_commands import (
 DWELL_TIME_SECONDS = MANAGER_PARAMS['dwell_time']
 TIME_OUT = MANAGER_PARAMS.get('timeout', 60)
 WS_PORT = int(os.environ.get('WS_PORT', 7799))
+thread_pool = ThreadPoolExecutor(max_workers=1)
+
+
+def do_crawl(driver, url):
+    driver.set_page_load_timeout(TIME_OUT)
+    try:
+        tab_restart_browser(driver)
+        driver.get(url)
+        # Sleep after get returns
+        time.sleep(DWELL_TIME_SECONDS)
+        success = True
+        failure_type = ''
+        message = ''
+    except TimeoutException as e:
+        logger.exception(e)
+        success = False
+        failure_type = 'timeout'
+        message = e.msg
+    except WebDriverException as e:
+        logger.exception(e)
+        success = False
+        failure_type = 'webdriver'
+        message = e.msg
+        if message.startswith('Reached error page:'):
+            try:
+                parsed = parse.urlparse(message.replace('Reached error page:', ''))
+                qs = parse.parse_qs(parsed.query)
+                failure_type = f'{parsed.path.strip()}:{qs["e"][0]}'
+            except:  # noqa
+                # OK if we can't get out this extra detail
+                pass
+    finally:
+        # How much problem that this isn't defensive?
+        close_modals(driver)
+        close_other_windows(driver)
+        driver.quit()
+    return success, failure_type, message
 
 
 @app.agent(crawl_request_topic)
@@ -32,43 +71,29 @@ async def crawl(crawl_requests):
     async for crawl_request in crawl_requests:
         print(f'Receiving Request: {crawl_request.url}')
         visit_id = (uuid.uuid4().int & (1 << 53) - 1) - 2**52
-        driver = get_driver(
-            visit_id=visit_id,
-            crawl_id=crawl_request.crawl_id,
-            ws_port=WS_PORT,
+        driver, logs = await app.loop.run_in_executor(
+            thread_pool,
+            partial(
+                get_driver,
+                data=dict(
+                    visit_id=visit_id,
+                    crawl_id=crawl_request.crawl_id,
+                    ws_port=WS_PORT,
+                )
+            )
         )
-        driver.set_page_load_timeout(TIME_OUT)
-        tab_restart_browser(driver)
-        try:
-            driver.get(crawl_request.url)
-            # Sleep after get returns
-            time.sleep(DWELL_TIME_SECONDS)
-            logger.info("Sleep complete")
-            success = True
-            failure_type = ''
-            message = ''
-        except TimeoutException as e:
-            logger.exception(e)
-            success = False
-            failure_type = 'timeout'
-            message = e.msg
-        except WebDriverException as e:
-            logger.exception(e)
-            success = False
-            failure_type = 'webdriver'
-            message = e.msg
-            if message.startswith('Reached error page:'):
-                try:
-                    parsed = parse.urlparse(message.replace('Reached error page:', ''))
-                    qs = parse.parse_qs(parsed.query)
-                    failure_type = f'{parsed.path.strip()}:{qs["e"][0]}'
-                except:  # noqa
-                    # OK if we can't get out this extra detail
-                    pass
-        finally:
-            close_modals(driver)
-            close_other_windows(driver)
-            driver.quit()
+        for log in logs:
+            logger.info(log)
+        success, failure_type, message = await app.loop.run_in_executor(
+            thread_pool,
+            partial(
+                do_crawl,
+                data=dict(
+                    driver=driver,
+                    url=crawl_request.url
+                )
+            )
+        )
         result = CrawlResult(
             request_id=crawl_request.request_id,
             visit_id=visit_id,
