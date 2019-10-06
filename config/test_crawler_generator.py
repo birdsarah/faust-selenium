1e85510e6e011a3d854c22a6ab1d68449ba6943a
Sarah Bird <birdsarah@mozilla.com>
2019-10-05 23:12:57 -0500

    Add test file for osx

---
+++ config/test_crawler_generator.py
@@ -0,0 +1,51 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+win_params = dict(
+    firefox_binary_path="C:\\Users\\Bird\\firefox-bin\\firefox.exe",
+    extension_path="C:\\Users\\Bird\\openwpm.xpi",
+    crawl_name_base='platform-comp-test-win-local',
+    database_name_base='data/platform-test-win-local',
+)
+linux_local_params = dict(
+    firefox_binary_path='/home/bird/Dev/birdsarah/faust-selenium/app/firefox-bin/firefox-bin',
+    extension_path='/home/bird/Dev/birdsarah/faust-selenium/app/openwpm.xpi',
+    crawl_name_base='platform-comp-test-linux-local',
+    database_name_base='data/platform-test-linux-local',
+    display=':99',
+)
+linux_cloud_params = dict(
+    firefox_binary_path='/home/sbird/faust-selenium/app/firefox-bin/firefox-bin',
+    extension_path='/home/sbird/faust-selenium/app/openwpm.xpi',
+    crawl_name_base='platform-comp-test-linux-cloud',
+    database_name_base='data/platform-test-linux-cloud',
+    display=':99',
+)
+osx_params = dict(
+    firefox_binary_path='/Users/caged/Dev/birdsarah/faust-selenium/Nightly.app/Contents/MacOS/firefox-bin',
+    extension_path='/Users/caged/Dev/birdsarah/faust-selenium/app/openwpm.xpi',
+    crawl_name_base='platform-comp-test-osx-local',
+    database_name_base='data/platform-test-osx-local',
+)
+
+
+def render():
+    e = Environment(
+        loader=(FileSystemLoader('templates')),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    t = e.get_template('crawler')
+    result = t.render(
+        browser_params_file='config/browser_params.json',
+        manager_params_file='config/manager_params.json',
+        n_crawlers=3,
+        p='test',
+        site_list='lists/alexatop1k.csv',
+        **osx_params
+    )
+    print(result)
+
+
+if __name__ == '__main__':
+    render()
---
+++ crawler/producer_test.py
@@ -0,0 +1,37 @@
+import csv
+import datetime
+import pytz
+import uuid
+
+from urllib.parse import quote
+
+from app import (
+    APPNAME,
+    app,
+    crawl_request_topic,
+    crawl_request_log_topic,
+    CrawlRequest,
+)
+
+CRAWL_ID = APPNAME
+
+@app.task
+async def producer():
+    with open('../lists/alexatop1k.csv', 'r') as f:
+        reader = csv.reader(f)
+        sites = [item for sublist in list(reader) for item in sublist]
+
+    sites = sites[0:10]
+    for u in sites:
+        url = quote(f'http://{u}', safe=":/?=")
+        request_id = (uuid.uuid4().int & (1 << 53) - 1) - 2**52
+        req = CrawlRequest(
+            url=url,
+            request_id=request_id,
+            crawl_id=CRAWL_ID,
+            time_stamp=str(datetime.datetime.now(pytz.utc))
+        )
+
+        print(f'Sending Request: {req.url}')
+        await crawl_request_log_topic.send(value=req)
+        await crawl_request_topic.send(value=req)
