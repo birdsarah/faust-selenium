8331cb5ddd60dc052650c180d8b02d274271d4bb
Sarah Bird <birdsarah@mozilla.com>
2019-10-05 20:05:27 -0500

    Add main crawler config

---
+++ config/crawler_generator.py
@@ -0,0 +1,31 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+FIREFOX_BINARY_PATH = "/home/sbird/faust-selenium/app/firefox-bin/firefox-bin"
+EXTENSION_PATH = "/home/sbird/faust-selenium/app/openwpm.xpi"
+
+
+def render():
+    e = Environment(
+        loader=(FileSystemLoader('templates')),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    t = e.get_template('crawler')
+    result = t.render(
+        browser_params_file='config/browser_params.json',
+        manager_params_file='config/manager_params.json',
+        firefox_binary_path=FIREFOX_BINARY_PATH,
+        extension_path=EXTENSION_PATH,
+        site_list='lists/alexatop1k.csv',
+        crawl_name_base='gcp-parallel-whead-kafka',
+        database_name_base='data/crawl-data',
+        display=':99',
+        n_crawlers=1,
+        n_parallel=16,
+    )
+    print(result)
+
+
+if __name__ == '__main__':
+    render()
---
+++ config/datasaver_generator.py
@@ -0,0 +1,26 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+FIREFOX_BINARY_PATH = "/home/sbird/faust-selenium/app/firefox-bin/firefox-bin"
+EXTENSION_PATH = "/home/sbird/faust-selenium/app/openwpm.xpi"
+
+
+def render():
+    e = Environment(
+        loader=(FileSystemLoader('templates')),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    t = e.get_template('datasaver')
+    result = t.render(
+        manager_params_file='config/manager_params.json',
+        crawl_name_base='gcp-parallel-whead-kafka',
+        database_name_base='data/crawl-data',
+        n_crawlers=1,
+        n_parallel=16,
+    )
+    print(result)
+
+
+if __name__ == '__main__':
+    render()
